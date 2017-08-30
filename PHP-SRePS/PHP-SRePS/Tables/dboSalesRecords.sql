433ccf0d85709bb8f7afbe261d95d5f018414338
Daniel Schurmann <2096633.student.swin.edu.au>
2017-08-30 23:56:29 +1000

    Database and tables set up

---
+++ PHP-SRePS/PHP-SRePS/Tables/dboSalesRecords.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleRecords]
+(
+	[SaleID] INT NOT NULL , 
+    [ProductID] INT NOT NULL, 
+    [Employee] NCHAR(10) NOT NULL, 
+    [SaleDate] DATE NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    PRIMARY KEY ([SaleID], [ProductID])
+)
