abe74421b98e51255b374fd9154add32a6c07da1
Casey-CT <100573942@student.swin.edu.au>
2017-09-07 12:06:01 +1000

    Added functions for connecting and disconnecting to database. Started working on SQL queries.

--- PHP-SRePS/PHP-SRePS/generateReports.cs
+++ PHP-SRePS/PHP-SRePS/generateReports.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 
 namespace PHP_SRePS
 {
     public partial class generateReports : Form
     {
+        SqlConnection connection;
+
         public generateReports()
         {
             InitializeComponent();
+        }
+
+        private void databaseConnect(SqlConnection con)
+        {
+            con = new SqlConnection();
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\PHP-SRePS.mdf;Integrated Security=true";
+            con.Open();
+        }
+
+        private void databaseDisconnect(SqlConnection con)
+        {
+            con.Close();
         }
 
         private void generateReports_Load(object sender, EventArgs e)
@@ -29,7 +46,18 @@ namespace PHP_SRePS
 
         private void weeklyReport_Click(object sender, EventArgs e)
         {
+            DateTime weekStart;
+            DateTime weekEnd;
 
+            weekStart = startDate.Value;
+            weekEnd = startDate.Value;
+            //Gonna need to fix this up later, will also have to format them correctly
+
+            databaseConnect(connection);
+            string scmd = "SELECT * FROM dbo.SaleRecords WHERE SaleDate BETWEEN " + weekStart + " AND " + weekEnd + ";";
+
+
+            databaseDisconnect(connection);
         }
 
         private void monthlyReport_Click(object sender, EventArgs e)
@@ -43,3 +71,5 @@ namespace PHP_SRePS
         }
     }
 }
+
+
